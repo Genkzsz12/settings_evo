f5771423cdf2f46ff43bf0f4cf2460d211afa65d
bigrushdog <randall.rushing@gmail.com>
2018-11-13 05:13:16 +0000

    Add Font preference for font manager
    Fix some stuff
    
    Change-Id: Ifb6eb578e6b6b22ca15baa7cb8f8db32bebe6e15

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -34,6 +34,7 @@ import com.android.settings.display.LiftToWakePreferenceController;
 import com.android.settings.display.NightDisplayPreferenceController;
 import com.android.settings.display.NightModePreferenceController;
 import com.android.settings.display.QsTileStylesPreferenceController;
+import com.android.settings.display.FontPickerPreferenceController;
 import com.android.settings.display.ScreenSaverPreferenceController;
 import com.android.settings.display.ShowOperatorNamePreferenceController;
 import com.android.settings.display.TapToWakePreferenceController;
@@ -102,6 +103,7 @@ public class DisplaySettings extends DashboardFragment {
                 KEY_AMBIENT_DISPLAY));
         controllers.add(new TapToWakePreferenceController(context));
         controllers.add(new TimeoutPreferenceController(context, KEY_SCREEN_TIMEOUT));
+		controllers.add(new FontPickerPreferenceController(context, lifecycle, fragment));
         controllers.add(new VrDisplayPreferenceController(context));
         controllers.add(new ShowOperatorNamePreferenceController(context));
         controllers.add(new WallpaperPreferenceController(context));
